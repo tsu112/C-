c00f191f8d334afb0a020a5d73d6e41752b2779b
Sue Yang <sueyang112@hotmail.com>
2022-08-02 23:19:06 -0400

    Adding my follow along on HelloSue and MathOperations.

---
+++ FollowAlongs/MathOperations/MathOperations.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(3 + 3);
+      Console.WriteLine("This is if you want to write a math equation and want to see the anwser only");
+      Console.WriteLine("3 + 3");
+      Console.WriteLine("This is if you want to write the equation out");
+    }
+  }
+}
